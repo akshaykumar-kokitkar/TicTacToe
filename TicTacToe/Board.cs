443963d2552c1bb3d47e134d266dbae9ef4bbefb
ak47 <kokitkarak@gmail.com>
2022-01-31 18:51:44 +0530

    tic tac toe 1st UC1

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        public Board()
+        {
+            char[] board = IntializedBoard();
+        }
+        
+        public char[] IntializedBoard()
+        {
+            char[] board = new char[10];
+
+            for (int index = 0; index < board.Length; index++)
+            {
+                board[index] = ' ';
+            }
+            return board;
+        }
+    }
+}
